d6b5aa0a2dd71159c9c3cae5729851cb85593bef
Charalampos Deligiannakis <49127082+iHaz32@users.noreply.github.com>
2023-04-04 19:47:44 +0300

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,10 @@
+export interface booksType { 
+    author: string
+    country: string
+    imageLink: string
+    language: string
+    link: string
+    pages: number
+    title: string
+    year: number 
+}
